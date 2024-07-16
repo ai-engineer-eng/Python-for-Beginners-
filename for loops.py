62c1afabcb1e92ffffb1c5794d2ce99e7782867d
ai-engineer-eng <wpexpert14@gmail.com>
2024-07-16 14:36:32 +0500

    to jump on next one

--- for loops.py
+++ for loops.py
@@ -1,6 +1,8 @@
+#to stop loop if list reach to "Apple"
 fruits = ["Banana", "Apple", "Mango"]
 for x in fruits:
 #to stop loop on specific item
     if x=="Apple":
-        break
+        #break
+        continue
     print(x)
