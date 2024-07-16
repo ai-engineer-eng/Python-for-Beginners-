0d6b7b294ff9b849ae6508221486d147b021d043
Shafqat <wpexpert14@gmail.com>
2024-07-16 16:44:36 +0500

    added new fruit

--- for loops.py
+++ for loops.py
@@ -1,5 +1,5 @@
 #to stop loop if list reach to "Apple"
-fruits = ["Banana", "Apple", "Mango"]
+fruits = ["Banana", "Apple", "Mango", "Mellon"]
 for x in fruits:
 #to stop loop on specific item
     if x=="Apple":
