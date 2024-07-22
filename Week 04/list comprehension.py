5a345f87e33b81490a0c257c8d274c30a209d5ae
Shafqat <wpexpert14@gmail.com>
2024-07-22 17:54:00 +0500

    updated

---
+++ Week 04/list comprehension.py
@@ -0,0 +1,17 @@
+#List comprehension offers a shorter syntax when you want to create a new list based on
+# the values of an existing list.
+#Based on a list of fruits, you want a new list, containing only the fruits with the letter
+# "a" in the name.
+#Without list comprehension you will have to write a for statement with a conditional test inside:
+
+fruits = ["apple", "banana", "cherry", "mango", "kiwi"]
+newlist = []
+# for x in fruits:
+#     if "a" in x:
+#         newlist.append(x)
+# print(newlist)
+
+#now with list comprehension method
+
+newlist = [x for x in fruits if "a" in x]
+print(newlist)
