916c071334d8fe0d9be4604f69c14799ad365844
Shafqat <wpexpert14@gmail.com>
2024-07-25 08:13:47 +0200

    updated

---
+++ Week 04/Sort Lists.py
@@ -0,0 +1,12 @@
+#List objects have a sort() method that will sort the list alphanumerically, ascending, 
+# by default:
+
+# players = ["Ronaldo", "Messi", "Mbape", "David"]
+# players.sort()
+# print(players)
+
+#sort the list numerically
+
+numbers = [100, 40, 38, 98, 23, 5]
+numbers.sort()
+print(numbers)
---
+++ Week 04/sort list numerically.py
@@ -0,0 +1,3 @@
+numbers = [100, 99, 88, 77, 66, 55, 44]
+numbers.sort()
+print(numbers)
