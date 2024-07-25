b0f82332ece4cb707f00d49d5b67295ec743babc
Shafqat <wpexpert14@gmail.com>
2024-07-25 11:16:11 +0200

    updated...

--- Week 04/Sort Lists.py
+++ Week 04/Sort Lists.py
@@ -7,7 +7,32 @@
 
 #sort the list numerically
 
-numbers = [100, 40, 38, 98, 23, 5]
+#numbers = [100, 40, 38, 98, 23, 5]
 # numbers.sort()
 # print(numbers)
 
+#To sort descending, use the keyword argument reverse = True:
+
+# numbers.sort(reverse=True)
+# print(numbers)
+
+#You can also customize your own function by using the keyword argument key = function.
+
+#The function will return a number that will be used to sort the list (the lowest number first):
+
+# def myfunc(n):
+#     return abs(n - 2)
+# numbers.sort(key=myfunc)
+# print(numbers)
+
+#By default the sort() method is case sensitive, resulting in all capital letters being
+# sorted before lower case letters:
+fruits = ["apple", "Banana", "Mango", "Duck"]
+# fruits.sort()
+# print(fruits)
+
+#So if you want a case-insensitive sort function, use str.lower as a key function:
+# fruits.sort(key=str.lower)
+# print(fruits)
+fruits.reverse()
+print(fruits)
