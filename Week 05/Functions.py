56353a95bac8e9c7fe7c2dcf5927fb50dcc6b6bf
Shafqat <wpexpert14@gmail.com>
2024-07-31 10:03:24 +0200

    updated function

--- Week 05/Functions.py
+++ Week 05/Functions.py
@@ -9,8 +9,33 @@
 
 
                             #keyword argument
-def number(num, by):
-    return num + by
+# def number(num, by):
+#     return num + by
 
 
-print(number(2, by=3))       #used keyword argument method to make code more readable
+# print(number(2, by=3))       #used keyword argument method to make code more readable
+
+
+                            #optional parameters method
+# def fullname(first_name, second_name="Khan"):       #defined a defualt argument to second_name parameter
+#     return f"Hi {first_name} {second_name}"
+
+
+# print(fullname("Shafqat", "Niazi"))                 #if we didn't pass here second argument,
+#                                                     #it will use defualt value, but if we pass it will 
+                                                    # take value of argument instead of by defualt value
+
+
+                                #Arbitrary Arguments, *args
+#If you do not know how many arguments that will be passed into your function, 
+# add a * before the parameter name in the function definition.
+# This way the function will receive a tuple of arguments, and can access the items accordingly:
+
+def sum(*numbers):
+    total = 1
+    for number in numbers:              #used for lop to iterate tuple
+
+        total = total + number
+    return total
+
+print(sum(2, 3, 4, 6, 7))               #it's output will be in the form of tuple
