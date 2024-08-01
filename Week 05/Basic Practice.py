bd504a9c0878fec714fd09c97c3f6d5ad3d54df6
Shafqat <wpexpert14@gmail.com>
2024-08-01 10:49:07 +0200

    avg calulator

---
+++ Week 05/Basic Practice.py
@@ -0,0 +1,11 @@
+                    #avarge calculator
+
+# def cal_avg(num1, num2, num3):        #defined a function; three parameters
+#     sum1 = num1+num2+num3         #make sum of three numbers and assign to a variable
+#     avg = sum1 / 3                #now average of numbers
+#     print(avg)
+
+# cal_avg(7, 94, 58)        #passing arguments; 3 numbers
+
+#......................................................................................................
+
