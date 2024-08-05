1dfd7bab961e72a0f0c6e31846d0f71bd0039ac7
Shafqat <wpexpert14@gmail.com>
2024-08-05 10:22:05 +0200

    updated

--- Week 05/Classes & OOP.py
+++ Week 05/Classes & OOP.py
@@ -1,5 +1,10 @@
 class Laptops:
-    RAM = "8 GB"
+    # RAM = "8 GB"
+    def __init__(self, name, RAM):
+        print("adding new student in database...")
+        self.name = name
+        self.RAM = RAM
 
-lap1 = Laptops()
-print(lap1.RAM)
+lap1 = Laptops("HP Elite Book - 3340", "8 GB")
+print(lap1.name, lap1.RAM)
+# print(lap1.RAM)
